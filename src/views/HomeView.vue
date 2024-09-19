c3888e2965435bd7cc78a4b644faecc2906d9e49
YUUKUMOTATSU <xiongyunlong134@gmail.com>
2024-09-19 22:24:58 +0900

    应用栏之选项卡菜单

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <template>
   <v-app>
-    <!-- 头部 -->
+    <!-- 应用栏区域 -->
     <v-app-bar app>
-      头部
+      <!-- 应用栏菜单选项 -->
+      <v-container fluid>
+        <v-tabs
+          slider-size="5"
+          centered
+        >
+          <v-tab 
+            v-for="tab in tabMenus"
+            :key="tab.id"
+            :to="tab.href"
+          >
+            {{ tab.name }}
+          </v-tab>
+        </v-tabs>
+      </v-container>
     </v-app-bar>
     <v-main>
       首页界面
@@ -14,5 +28,26 @@
 
 export default {
   name: 'Home',
+  data: () => ({
+    tabMenus: [],
+  }),
+  created() {
+    // 获取应用栏菜单
+    this.getAppBarMenus()
+    console.log('tabMenus', this.tabMenus)
+  },
+  methods: {
+    // 获取应用栏菜单
+    getAppBarMenus() {
+      // 请求服务器 -- 获取 -- 获取应用栏菜单
+      this.tabMenus = [
+        { id: 1, name: '首页', href: '/'},
+        { id: 2, name: '博客', href: '/blog'},
+        { id: 3, name: '学堂', href: '/school'},
+        { id: 4, name: '影音', href: '/film'},
+        { id: 5, name: '考证', href: '/exam'},
+      ]
+    }
+  },
 }
 </script>
