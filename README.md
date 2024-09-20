c82645b9b7edc4e36e303c12d9ed7e7c5fe1bf8b
YUUKUMOTATSU <xiongyunlong134@gmail.com>
2024-09-20 13:51:37 +0900

    页脚链接

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue2_vuetify2_pj
+# vue2-zhifou
 
 ## Project setup
 ```
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -68,6 +68,12 @@
         </v-card>
       </v-container>
     </v-main>
+    <!-- 网站页脚 -->
+    <v-footer>
+      <v-container fluid class="d-flex justify-center">
+        <v-chip small lable color="transparent" v-for="link in footerLinks" :to="link.to">{{ link.text }}</v-chip>
+      </v-container>
+    </v-footer>
   </v-app>
 </template>
 
@@ -79,6 +85,7 @@ export default {
     tabMenus: [], //应用栏菜单
     homeBackPic: '', // 首页背景图
     homeCards: [], // 主页卡片信息
+    footerLinks: [], // 页脚链接
   }),
   created() {
     // 获取应用栏菜单
@@ -87,6 +94,8 @@ export default {
     this.getHomeBackPic()
     // 获取卡片信息
     this.getHomeCards()
+    // 获取网页链接
+    this.getFooterLinks()
   },
   methods: {
     // 获取首页的背景图
@@ -107,6 +116,7 @@ export default {
     },
     // 获取主页的卡片信息
     getHomeCards() {
+      // 请求服务器 -- 获取 -- 获取主页的卡片信息
       this.homeCards = [
         {
           id: 1,
@@ -181,6 +191,21 @@ export default {
             "微软认证",
           ],
         },
+      ]
+    },
+    // 获取页脚链接
+    getFooterLinks() {
+      // 请求服务器  -->  获取  -->  获取页脚链接
+      this.footerLinks = [
+        { id: 1, text: '增值电信业务经营许可证：合字B2-20090007', to: '' },
+        { id: 2, text: '京ICP备10036305号-7', to: '' },
+        { id: 3, text: '京公网安备11010802022657号', to: '' },
+        { id: 4, text: '隐私声明和 Cookie', to: '' },
+        { id: 5, text: '法律声明', to: '' },
+        { id: 6, text: '广告', to: '' },
+        { id: 7, text: '帮助', to: '' },
+        { id: 8, text: '反馈', to: '' },
+        { id: 9, text: '© 2022 Microsoft', to: '' },
       ]
     },
   },
